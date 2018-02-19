a66673ded4d3a84c1bd52bcf1957789f9f6f3547
MiniDigger <mini.digger@live.de>
2018-02-19 09:35:07 +0100

    Create deploy-build-results.sh

---
+++ scripts/deploy-build-results.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+echo "clean deploy folder"
+rm -rf deploy-stuff
+
+echo "setup git"
+git config --global user.email "vglbot@minidigger.me"
+git config --global user.name "VoxelGamesLibBot"
+
+echo "clone repo"
+git clone --depth 10 -b gh-pages "https://${GITHUB_TOKEN}@github.com/VoxelGamesLib/docs.git" deploy-stuff
+
+echo "copy stuff to deploy"
+cp -R site deploy-stuff
+
+echo "add to repo"
+cd deploy-stuff
+git add .
+echo "commit"
+git commit -m "Deploy to Github Pages (docs)"
+echo "push"
+git push --force "https://${GITHUB_TOKEN}@github.com/VoxelGamesLib/docs.git" gh-pages:gh-pages
+echo "DONE"
